62a98cd17c2bea60f00367b976c9f2c95c71d43f
张海印 <yeshen1635@163.com>
2022-03-21 22:29:08 +0800

    feat：增加一个插件工具

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -128,7 +128,25 @@ module.exports = {
       clientSecret: '28d7f5d3d6fbc1405a3c57a833b061b32e1f7d4f',
       locale: 'zh', //使用的语言  这里是简体中文
       baseURL: 'https://github.com'
-    }] //平台的 base URL
+    }], //平台的 base URL
+    ["vuepress-plugin-nuggets-style-copy", {
+      copyText: "复制代码",
+      tip: {
+          content: "复制成功"
+      }
+   }],
+   [
+    '@vuepress-reco/vuepress-plugin-kan-ban-niang',
+    {
+      theme: ['blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16']
+    }
+  ],
+  ['cursor-effects', {
+    size: 2, // size of the particle, default: 2
+    shape: 'star', // ['star' | 'circle'], // shape of the particle, default: 'star'
+    zIndex: 999999999, // z-index property of the canvas, default: 999999999
+	}]
+
   ]
 
 
